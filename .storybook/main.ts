1ebd7babc07199d098980539f33a71f69182bbf3
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-09-23 21:08:39 +0100

    Updated configs to allow SVG Imports

---
+++ .storybook/main.ts
@@ -0,0 +1,37 @@
+import type { StorybookViteConfig } from "@storybook/builder-vite";
+import { mergeConfig } from "vite";
+
+import svgr from "vite-plugin-svgr";
+
+let config: StorybookViteConfig = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      plugins: [svgr({ exportAsDefault: true })],
+    });
+  },
+};
+
+export default config;
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,1 @@
-declare module "*.svg" {
-  const content: React.FC<React.SVGProps<SVGElement>>;
-  export default content;
-}
-
 declare type setState<T> = React.Dispatch<React.SetStateAction<T>>;

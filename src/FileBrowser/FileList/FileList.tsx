cb82595dcdaa0af7b38ef350ba2d6407529b3257
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2022-10-04 21:56:26 +0100

    re-worked fileBrowser
    - svgs are formatted
    - css styles are refactored and improved.

--- src/FileBrowser/FileList/FileList.tsx
+++ src/FileBrowser/FileList/FileList.tsx
@@ -14,33 +14,25 @@ let FileList: FC<fileListProps> = ({ data, update }) => {
   let keys = Object.keys(data)
     .sort()
     .sort((a, b) => {
-      let valA = typeof data[a] === typeof "" ? 0 : 1;
-      let valB = typeof data[b] === typeof "" ? 0 : 1;
+      let valA = typeof data[a] === "string" ? 0 : 1;
+      let valB = typeof data[b] === "string" ? 0 : 1;
       return valB - valA;
     });
 
   return (
     <ul className={styles.list}>
-      {keys.map((each) => {
-        let icon = <Folder className={styles.svg} />;
-        if (typeof data[each] == typeof "") {
-          icon = <File className={styles.svg} />;
-        }
-
-        return (
-          <li key={each} className={styles.item}>
-            <button
-              onClick={() => {
-                update(each);
-              }}
-              className={styles.fileButton}
-            >
-              {icon}
-              <h3>{each}</h3>
-            </button>
-          </li>
-        );
-      })}
+      {keys.map((each) => (
+        <li key={each} className={styles.item}>
+          <button onClick={() => update(each)} className={styles.fileButton}>
+            {typeof data[each] === "string" ? (
+              <File className={styles.svg} />
+            ) : (
+              <Folder className={styles.svg} />
+            )}
+            <h3>{each}</h3>
+          </button>
+        </li>
+      ))}
     </ul>
   );
 };
